d4d62bd3775a80face985f37a89bbb3cd1b969fd
Gian <gianmnf@gmail.com>
2024-09-26 14:31:27 -0300

    Dependency Injection v1

---
+++ di/di_test.go
@@ -0,0 +1,18 @@
+package di
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Gian")
+
+	got := buffer.String()
+	want := "Hello, Gian"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
